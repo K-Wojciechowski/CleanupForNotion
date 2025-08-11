344bbec53180e5977b12e8a87c984bf727d13358
Krzysztof Wojciechowski <kw@krzysztofwojciechowski.pl>
2025-08-11 20:33:03 +0200

    Terraform: add tags to all resources, add region to invoke scripts

--- aws-terraform/scripts/main.tf
+++ aws-terraform/scripts/main.tf
@@ -1,11 +1,11 @@
 resource "local_file" "bash" {
-  content         = "#!/bin/bash\naws lambda invoke --function-name ${var.lambda_name} /dev/null\n"
+  content         = "#!/bin/bash\naws lambda invoke --region ${var.aws_region} --function-name ${var.lambda_name} /dev/null\n"
   filename        = "${path.root}/invoke.sh"
   file_permission = "0755"
 }
 
 resource "local_file" "cmd" {
-  content         = "aws lambda invoke --function-name ${var.lambda_name} NUL\r\n"
+  content         = "aws lambda invoke --region ${var.aws_region} --function-name ${var.lambda_name} NUL\r\n"
   filename        = "${path.root}/invoke.cmd"
   file_permission = "0755"
 }
